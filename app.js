6f94e497304e3606d0d7819f6019bf0d9f0b1765
vlad kuznetsov <vlad.y.kuznetsov@gmail.com>
2020-02-17 23:14:35 -0500

    ES6 budget controller changes

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
-    
-    var Expense = function(id, description, value) {
+
+    class Expense {
+        constructor (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
         this.percentage = -1;
-    };
+        }
+
+        calcPercentage(totalIncome) {
+            if (totalIncome > 0) {
+                this.percentage = Math.round((this.value / totalIncome) * 100);
+            } else {
+                this.percentage = -1;
+            }
+        }
+
+        getPercentage() {
+            return this.percentage;
+        }
+    }
+    
+    /*var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
+    };*/
     
     
-    Expense.prototype.calcPercentage = function(totalIncome) {
+    /*Expense.prototype.calcPercentage = function(totalIncome) {
         if (totalIncome > 0) {
             this.percentage = Math.round((this.value / totalIncome) * 100);
         } else {
             this.percentage = -1;
         }
-    };
+    };*/
     
     
-    Expense.prototype.getPercentage = function() {
+    /*Expense.prototype.getPercentage = function() {
         return this.percentage;
-    };
+    };*/
     
     
     var Income = function(id, description, value) {
